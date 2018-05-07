6a5987e5c41112d71555d9f566ea25752e4a392c
Gustavo Monteiro <monteirogustavop@gmail.com>
2018-05-07 15:30:22 -0300

    Split sample in train, valid and test.

--- 2prog/svr.R
+++ 2prog/svr.R
@@ -4,28 +4,34 @@ library(dplyr)
 #Variável Dependente
 y <- as.numeric(dados_oceania$percent)
 y[2] <- 0
-y <- na.omit(y)
+y <- y[-1]
 y.lag <- as.numeric(dados_oceania$percent)
-y.lag[1:2] <- 0
+y.lag <- y.lag[-1]
+
 
 #Variável Independente 
 colunas <- c(3:6)
 x <- subset(dados_oceania, select = colunas)
 X <- as.matrix(cbind(y.lag, x))
-ids<-sample(1:nrow(dados_oceania),0.7*nrow(dados_oceania))
+ids <- 1:nrow(dados_oceania)
+ids_train <- sample(ids, 0.7*nrow(dados_oceania))
+ids_valid <- ids[-ids_train]
+ids_valid <- sample(ids_valid, 0.2*nrow(dados_oceania))
+ids_test <- ids[-ids_valid]
+ids_test <- sample(ids_test, 0.1*nrow(dados_oceania))
 
 #Training Process
-a.Vet<-seq(0.1,5,0.05)
-MSE<-1e+10
+a.Vet <- seq(0.1,5,0.05)
+MSE <- 1e+10
 R <- length(a.Vet)
 
-suportVector<-data.frame("Kernel"="Mexican","Parameter1"=NA,"MSE"=NA)
+suportVector <- data.frame("Kernel" = "Mexican", "Parameter1" = NA, "MSE" = NA)
 for(i in 1:R){
   #Mexican Kernel parameter
   a<-a.Vet[i]
   
   #Kernel matrix
-  K<-as.kernelMatrix(apply(X,1,function(x) apply(X,1,function(y) prod((1-(((x-y)^2)/(a^2)))*exp(-((x-y)^2)/(2*a))))))
+  K<-as.kernelMatrix(apply(X, 1, function(x) apply(X, 1, function(y) prod((1-(((x-y)^2)/(a^2)))*exp(-((x-y)^2)/(2*a))))))
   
   #Training Kernel
   K.train <- as.kernelMatrix(K[-ids,-ids]) 
